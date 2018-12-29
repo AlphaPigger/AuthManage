3073756566813a69df4652deefae9f3cc69008df
林青 田 <792085369@qq.com>
2018-12-29 09:29:06 +0800

    完成业务呈现部分（包括导航栏和具体按钮）

---
+++ AuthManage.MVC/Components/HardwareEditViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using AuthManage.Application.DTOModel;
+using AuthManage.Application.IAppServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthManage.MVC.Components
+{
+    [ViewComponent(Name ="HardwareEdit")]
+    public class HardwareEditViewComponent:ViewComponent
+    {
+        //依赖注入
+        private IMenuAppService _menuAppService;
+        //构造函数
+        public HardwareEditViewComponent(IMenuAppService menuAppService)
+        {
+            _menuAppService = menuAppService;
+        }
+        public IViewComponentResult Invoke(int hardwareID)
+        {
+            ViewBag.hardwareID = hardwareID;
+            List<MenuDto> menuDtos = new List<MenuDto>();
+            string currentUser = HttpContext.Session.GetString("CurrentUser");
+            if (currentUser == "admin")//如果为超级管理员
+            {
+                menuDtos = _menuAppService.GetAllDtos();
+            }
+            else//普通用户
+            {
+                var currentUserID = HttpContext.Session.GetString("CurrentUserID");
+                menuDtos = _menuAppService.GetMenusByUser(int.Parse(currentUserID));
+            }
+            return View(menuDtos);
+        }
+    }
+}
